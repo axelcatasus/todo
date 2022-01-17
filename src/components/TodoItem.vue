4885259aada8b5c2b2d349eee546ca12b659e30d
axelcatasus <84475869+axelcatasus@users.noreply.github.com>
2022-01-17 22:40:55 +0100

    hej

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li>
+    <input type="checkbox" @click="$emit('checked')" v-model="item.done">
+    <h1>{{item.content}}</h1>
+  </li>
+</template>
+
+<script>
+export default {
+  data(){return{
+    isChecked: false
+  }},
+  props: {item: Object},
+  methods: {}
+}
+</script>
+
+<style scoped>
+  li{
+    display:flex;
+    padding: 0;
+    margin: 0;
+  }
+  input {
+    accent-color: rebeccapurple;
+  }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <ul>
+          <TodoItem v-for="item in todoItems" :key="item.id" :item="item" @checked="checkCheck(item)"/>
+      </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem.vue"
+export default {
+    name: 'TodoList',
+    components: {TodoItem},
+    props: ['todoItems'],
+    methods: {
+        checkCheck(item){
+            this.$emit('checked', item)
+        }
+    }
+}
+
+
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
+</style>
